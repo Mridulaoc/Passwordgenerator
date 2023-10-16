9ae37508111616db1e81019943d6938386f46d23
Mridulaoc <112925003+Mridulaoc@users.noreply.github.com>
2023-10-16 12:06:09 +0530

    Password Generator

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
@@ -9,14 +9,16 @@ function App() {
   const[characterAllowed,setCharacterAllowed]=useState(false)
   const [password,setPassword] = useState("")
 
+  const passwordRef = useRef(null)
+
   const passwordGenerator= useCallback(()=>{
 
     let genPass=""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     if(numberAllowed) str += "0123456789"
-    if(characterAllowed) str +="!@#$%^&*-_+=[]{}~`"
+    if(characterAllowed) str +="!@#$%^&*-_+="
 
-    for(let i=0; i<=length; i++){
+    for(let i=1; i<=length; i++){
       let character = Math.floor(Math.random() * str.length + 1)
       genPass += str.charAt(character)
     }
@@ -25,11 +27,17 @@ function App() {
 
   },[length,characterAllowed,numberAllowed,setPassword])
 
+  const copyPassword = useCallback(()=>{
+   passwordRef.current?.select()
+   passwordRef.current?.setSelectionRange(0,999)
+   window.navigator.clipboard.writeText(password)
+  })
+
   
 
   useEffect(()=>{
     passwordGenerator()
-  },[length,characterAllowed,numberAllowed,setPassword])
+  },[length,characterAllowed,numberAllowed,passwordGenerator])
 
 
 
@@ -38,20 +46,30 @@ function App() {
     <div className='w-6/12 h-auto bg-gray-800 mt-10 m-auto'>
       <h1 className='uppercase font-semibold text-orange-600'>password generator</h1>
       <div className='flex flex-col justify-center md:flex-row'>
-        <input type="text" placeholder='password' value={password}  className='w-8/12'/>
-        <button className='bg-blue-800 text-white'>Copy</button>
+        <input type="text" placeholder='password' value={password}  className='w-8/12' readOnly ref={passwordRef}/>
+        <button className='bg-blue-800 text-white' onClick={copyPassword}>Copy</button>
       </div>
       <div className='flex flex-col justify-between px-7 py-4 md:flex-row'>
         <div className='flex gap-1 text-white'>
-        <input type="range" min={6} max={100} value={length}/>
+        <input
+         type="range"
+         min={6} max={100}
+         value={length}
+         onChange={(e)=>{setLength(e.target.value)}}
+         className='cursor-pointer'/>
         <label>Length:{length}</label>
         </div>
         <div className='flex gap-1  text-white'>
-          <input type="checkbox" />
+          <input type="checkbox"
+           defaultChecked={numberAllowed} 
+           onChange={()=>{setNumberAllowed((prev)=>!prev)}} />
           <label htmlFor="numberInput">Number</label>
         </div>
         <div className='flex gap-1  text-white'>
-          <input type="checkbox" />
+          <input 
+          type="checkbox" 
+          defaultChecked={characterAllowed} 
+          onChange={()=>{setCharacterAllowed((prev)=>!prev)}}/>
           <label htmlFor="characterInput">Character</label>
         </div>
 
